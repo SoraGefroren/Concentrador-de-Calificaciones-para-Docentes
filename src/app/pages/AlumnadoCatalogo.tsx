df147e598fbd40b82aba3060cb3c223bcf9db12e
Jorge L Jácome Domínguez <leon.blanco@hotmail.com>
2025-05-31 13:29:49 -0600

    Ajustes

--- src/app/pages/AlumnadoCatalogo.tsx
+++ src/app/pages/AlumnadoCatalogo.tsx
@@ -6,6 +6,7 @@ import { Column } from 'primereact/column';
 
 const AlumnadoCatalogo = () => {
     const { excelData } = useExcelContext();
+
     const chooseBodyTemplate = (excelColumn: string) => {
         // Retornamos una función que recibe el rowData y rowIndex
         return (rowData: ExcelData, props: { rowIndex: number }) => {
@@ -13,156 +14,86 @@ const AlumnadoCatalogo = () => {
             const { rowIndex } = props;
             // Para la primera fila
             if (rowIndex === 0) {
-                if (excelColumn !== 'NOMBRE') {
-                    return rowZeroContentDateTemplate(rowData, { field: excelColumn, rowIndex });
-                } else {
-                    return null;
-                }
+                // Si el campo es una fecha, lo convertimos a formato DD/MM/YYYY
+                return rowZeroContentDateTemplate(rowData, { field: excelColumn, rowIndex });
             // Para la segunda fila
             } else if (rowIndex === 1) {
-                if (excelColumn !== 'NOMBRE') {
-                    return rowOneContentDateTemplate(rowData, { field: excelColumn, rowIndex });
-                } else {
-                    return null;
-                }
+                // Si el campo es un número, le damos formato
+                return rowOneContentDateTemplate(rowData, { field: excelColumn, rowIndex });
             // Todas las demás filas
             } else {
-                switch (excelColumn) {
-                    case '1ER.PARCIAL-30-SEP-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case '2DO.PARCIAL-19-NOV-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'APELLIDO':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'BUSQUEDA':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'CALIFICACION':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'CLASIFICACION.METODOS.ECUACIONES-CUADRATICAS-14-ENE-22':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'CONCEPTOS.BASICOS.PROBABILIDAD-05-NOV-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'CONTEXTO ALGEBRAICO-12-NOV-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'CORREO.ELECTONICO':
-                    case 'CORREO.ELECTONICO ':
-                    case '"CORREO.ELECTONICO "':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'DIVISION.POLINOMIOS-03-DIC-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'ECUACION.LINEAL.DOS-VARIABLES-17-DIC-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'ECUACION.LINEAL.TRES-VARIABLES-07-ENE-22':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'ECUACION.LINEAL.UNA-VARIABLE-10-DIC-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'EXAMEN.FINAL-19-ENE-22':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'ID':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'ID2':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'JERARQUIA-OPERACIONES-MCM-MCD-10-SEP-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'MEDIDAS.DISPERSION-29-OCT-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'MEDIDAS.TENDENCIA.CENTRAL-22-OCT-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'MULTIPLICACION.POLINOMIOA-26-NOV-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'NOMBRE':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'PARTICIPACIÓN':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'PRESEN-ENCUADRE-27-AGO-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'PROPIEDADES-NUMEROS-REALES-03-SEP-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'PROPORCIONALIDAD-17-SEP-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'SUCESION-SERIES-01-OCT-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'SUCESIONES-SERIES-ARITMETICAS-08-OCT-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'SUCESIONES-SERIES-GEOMETRICAS-15-OCT-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'SUMA-RESTA-POLINOMIOS-19-NOV-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'SUMA.PORCENTAJE.ACTIVIDADES':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'TOTAL.ALCANZADO':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'TOTAL.ALCANZADO.DE.PORCENTAJE.ACTIVIDADES':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    case 'VARIACION.DIRECTA-INVERSA-24-SEP-21':
-                        return statusBodyTemplate(rowData, { field: excelColumn, rowIndex });
-                        break;
-                    default:
-                        return null;
-                        break;
+                 if (['ID', 'ID2'].includes(excelColumn)) {
+                    return columnContentValueIDTemplate(rowData, { field: excelColumn, rowIndex });
+                } else if (['CORREO.ELECTONICO', 'CORREO.ELECTONICO ', '"CORREO.ELECTONICO "'].includes(excelColumn)) {
+                    return columnContentValueMailTemplate(rowData, { field: excelColumn, rowIndex });
+                } else if (['NOMBRE', 'APELLIDO'].includes(excelColumn)) {
+                    return  <div className="w-full text-left font-semibold">
+                                { rowData[excelColumn] || '' }
+                            </div>;
+                } else {
+                    return  !['BUSQUEDA'].includes(excelColumn)
+                                ?   <div className="w-full text-right">
+                                        { 
+                                            new Intl.NumberFormat('en-US', {
+                                                minimumFractionDigits: 2,
+                                                maximumFractionDigits: 2,
+                                            }).format(
+                                                parseFloat((rowData[excelColumn] || '0') + '')
+                                            )
+                                        }
+                                    </div>
+                                :   null;
                 }
             }
         };
     };
     
-    const rowZeroContentDateTemplate = (rowData: ExcelData, props: { field: string, rowIndex: number }, xx) => {
+    const rowZeroContentDateTemplate = (rowData: ExcelData, props: { field: string, rowIndex: number }) => {
+        // Si el campo es una fecha, lo convertimos a formato DD/MM/YYYY
         if (rowData[props.field] && (rowData[props.field] !== 'Fecha')) {
             // Excel cuenta desde 1 de enero de 1900, pero tiene un bug que considera 1900 como bisiesto
             const excelEpoch = new Date(1899, 11, 30); // 30 de diciembre de 1899
             const date = new Date(excelEpoch.getTime() + (parseFloat((rowData[props.field] || '0') + '')) * 24 * 60 * 60 * 1000);
             // Formato DD/MM/YYYY
-            const day = String(date.getDate()).padStart(2, '0');
-            const month = String(date.getMonth() + 1).padStart(2, '0');
-            const year = date.getFullYear();
-            return  <div>
-                        { `${day}/${month}/${year}`}
+            return  <div className="w-full text-right font-bold">
+                        { `${String(date.getDate()).padStart(2, '0')}/${String(date.getMonth() + 1).padStart(2, '0')}/${date.getFullYear()}`}
                     </div>;
         } else {
-            return  <div>
+            // Etiqueta de fecha vacía o no válida
+            return  <div className="w-full text-right font-bold">
+                        { rowData[props.field] || '' }
                     </div>;
         }
     };
-    const rowOneContentDateTemplate = (rowData: ExcelData, props: { field: string, rowIndex: number }, xx) => {
-        return  <div>
-                    { JSON.stringify(rowData[props.field]) }
+    const rowOneContentDateTemplate = (rowData: ExcelData, props: { field: string, rowIndex: number }) => {
+        // Si el campo es una fecha, lo convertimos a formato DD/MM/YYYY
+        if (rowData[props.field] && (rowData[props.field] !== 'Puntos')) {
+            return  <div className="w-full text-right font-bold">
+                        { rowData[props.field] || 0 }
+                    </div>;
+        } else {
+            // Etiqueta de fecha vacía o no válida
+            return  <div className="w-full text-right font-bold">
+                        { rowData[props.field] || '' }
+                    </div>;
+        }
+    };
+    const columnContentValueIDTemplate = (rowData: ExcelData, props: { field: string, rowIndex: number }) => {
+        return  <div className="w-full text-right font-bold">
+                    { rowData[props.field] || 0 }
                 </div>;
     };
-    const statusBodyTemplate = (rowData: ExcelData, props: { field: string, rowIndex: number }, xx) => {
-        return  <div>
-                    { JSON.stringify(rowData[props.field]) }
-                </div>;
+    const columnContentValueMailTemplate = (rowData: ExcelData, props: { field: string, rowIndex: number }) => {
+        if (rowData[props.field]) {
+            return  <div>
+                        <a href={`mailto:${rowData[props.field]}`}
+                            className="text-blue-600 underline hover:text-blue-800 transition-colors duration-200">
+                            { rowData[props.field] }
+                        </a>
+                    </div>;
+        } else {
+            return null;
+        }
     };
 
     const getRowClassName = (data: ExcelData) => {
@@ -173,7 +104,6 @@ const AlumnadoCatalogo = () => {
         // Resto de las filas
         return '';
     };
-    
     
     return (
         <Menu>
@@ -186,6 +116,7 @@ const AlumnadoCatalogo = () => {
                            tableStyle={{ minWidth: '100%', maxWidth: '100%' }}>
                     {excelData.length > 0 &&
                         Object.keys(excelData[2])
+                            .filter(key => key !== "BUSQUEDA")
                             .map((col, index) => (
                                 <Column key={index}
                                         field={col} header={col}
